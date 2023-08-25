f4d28da15a40551060967fdfa866232ae875cbbd
Silzinc <jonasbloch85@gmail.com>
2023-08-26 01:38:03 +0200

    Simplified and refactored Neg, Sub and SubAssign

--- src/ops/add.rs
+++ src/ops/add.rs
@@ -1,10 +1,7 @@
-use crate::Polynomial;
-use crate::Polynomial::{NonZero, Zero, X};
-use num_traits::ToPrimitive;
-use std::{
-	fmt::Debug,
-	ops::{Add, AddAssign, Neg, Sub, SubAssign},
-};
+use crate::Polynomial::{self, NonZero, Zero, X};
+use duplicate::duplicate;
+use num_traits::{Float, ToPrimitive};
+use std::ops::{Add, AddAssign, Neg, Sub, SubAssign};
 
 fn convert<T1: ToPrimitive, T2: Float>(x: T1) -> T2 { T2::from(x).unwrap() }
 
@@ -69,16 +66,6 @@ duplicate! {
 		type Output = Polynomial<T>;
 		fn add(self, other: Added) -> Polynomial<T> { self_reference + other_reference }
 	}
-impl Add<Polynomial> for Polynomial
-{
-	type Output = Polynomial;
-
-	fn add(self, other: Polynomial) -> Polynomial { &self + &other }
-}
-impl Add<Polynomial> for &Polynomial
-{
-	type Output = Polynomial;
-	fn add(self, other: Polynomial) -> Polynomial { self + &other }
 }
 
 // Number adding versions
@@ -146,154 +133,126 @@ where
 	fn add_assign(&mut self, other: T) { *self = *self + other }
 }
 
-// Negating a polynomial
+// Negating a Polynomial
 // ========================================================================================
 
-impl Neg for &Polynomial
+impl<T: Float> Neg for &Polynomial<T>
 {
-	// Implements negation (a copy of the polynomial is created when this is called)
-	type Output = Polynomial;
+	// Implements negation (a copy of the Polynomial<T> is created when this is
+	// called)
+	type Output = Polynomial<T>;
 
-	fn neg(self) -> Polynomial
+	fn neg(self) -> Polynomial<T>
 	{
 		match self {
 			Zero => Zero,
-			X => NonZero(vec![0., -1.]),
+			X => NonZero(vec![convert(0), convert(-1)]),
 			NonZero(coefs) => {
-				let mut new_coefs = vec![0f64; coefs.len()];
+				let mut new_coefs = vec![convert(0); coefs.len()];
 				for index in 0..new_coefs.len() {
 					new_coefs[index] = -coefs[index];
 				}
 				NonZero(new_coefs)
-			}
+			},
 		}
 	}
 }
-impl Neg for Polynomial
+impl<T: Float> Neg for Polynomial<T>
 {
 	// Version that takes ownership
-	type Output = Polynomial;
-	fn neg(self) -> Polynomial { -&self }
+	type Output = Polynomial<T>;
+	fn neg(self) -> Polynomial<T> { -&self }
 }
 
 // Subtraction
 // =================================================================================================
 
-impl Sub<&Polynomial> for &Polynomial
+impl<T: Float + AddAssign> Sub<&Polynomial<T>> for &Polynomial<T>
 {
 	// Implements subtraction
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { self + (-other) }
+	type Output = Polynomial<T>;
+	fn sub(self, other: &Polynomial<T>) -> Polynomial<T> { self + (-other) }
 }
 
 // Ownership taking versions
 
-impl Sub<Polynomial> for &Polynomial
+impl<T: Float + AddAssign> Sub<Polynomial<T>> for &Polynomial<T>
 {
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { self + (-&other) }
+	type Output = Polynomial<T>;
+	fn sub(self, other: Polynomial<T>) -> Polynomial<T> { self + (-&other) }
 }
-impl Sub<Polynomial> for Polynomial
+impl<T: Float + AddAssign> Sub<Polynomial<T>> for Polynomial<T>
 {
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { &self + (-&other) }
+	type Output = Polynomial<T>;
+	fn sub(self, other: Polynomial<T>) -> Polynomial<T> { &self + (-&other) }
 }
-impl Sub<&Polynomial> for Polynomial
+impl<T: Float + AddAssign> Sub<&Polynomial<T>> for Polynomial<T>
 {
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { &self + (-other) }
+	type Output = Polynomial<T>;
+	fn sub(self, other: &Polynomial<T>) -> Polynomial<T> { &self + (-other) }
 }
 
-// Subtracting floats 32 bits to polynomials
-// ========================================================================
+// Number subtracting versions
 
-impl<T: ToPrimitive + Debug> Sub<T> for Polynomial
+impl<F, T> Sub<T> for Polynomial<F>
+where
+	F: Float + AddAssign,
+	T: ToPrimitive,
 {
-	type Output = Polynomial;
-	fn sub(self, other: T) -> Polynomial
+	type Output = Polynomial<F>;
+	fn sub(self, other: T) -> Polynomial<F>
 	{
-		let x: f64 = match other.to_f64() {
-			Some(y) => y,
-			None => panic!("Error when converting {:?} to f64", other),
-		};
-		&self + Polynomial::from(vec![-x])
+		let x: F = convert(other);
+		&self + Polynomial::<F>::from(vec![-x])
 	}
 }
-impl<T: ToPrimitive + Debug> Sub<T> for &Polynomial
+impl<F, T> Sub<T> for &Polynomial<F>
+where
+	F: Float + AddAssign,
+	T: ToPrimitive,
 {
-	type Output = Polynomial;
-	fn sub(self, other: T) -> Polynomial
+	type Output = Polynomial<F>;
+	fn sub(self, other: T) -> Polynomial<F>
 	{
-		let x: f64 = match other.to_f64() {
-			Some(y) => y,
-			None => panic!("Error when converting {:?} to f64", other),
-		};
-		self + Polynomial::from(vec![-x])
+		let x: F = convert(other);
+		self + Polynomial::<F>::from(vec![-x])
 	}
 }
-impl Sub<Polynomial> for f64
-{
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { (-other) + self }
-}
-impl Sub<&Polynomial> for f64
-{
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { (-other) + self }
-}
-impl Sub<Polynomial> for f32
-{
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { (-other) + (self as f64) }
-}
-impl Sub<&Polynomial> for f32
-{
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { (-other) + (self as f64) }
-}
-impl Sub<Polynomial> for i32
-{
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { (-other) + (self as f64) }
-}
-impl Sub<&Polynomial> for i32
-{
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { (-other) + (self as f64) }
-}
-impl Sub<Polynomial> for i64
-{
-	type Output = Polynomial;
-	fn sub(self, other: Polynomial) -> Polynomial { (-other) + (self as f64) }
-}
-impl Sub<&Polynomial> for i64
-{
-	type Output = Polynomial;
-	fn sub(self, other: &Polynomial) -> Polynomial { (-other) + (self as f64) }
+
+duplicate! {
+	[primitive_type; [f64]; [f32]; [i8]; [i16]; [i32]; [i64]; [isize]; [i128]; [u8]; [u16]; [u32]; [u64]; [usize]; [u128]]
+
+	impl<T: Float + AddAssign> Sub<Polynomial<T>> for primitive_type
+	{
+		type Output = Polynomial<T>;
+		fn sub(self, other: Polynomial<T>) -> Polynomial<T> { (-other) + self }
+	}
+	impl<T: Float + AddAssign> Sub<&Polynomial<T>> for primitive_type
+	{
+		type Output = Polynomial<T>;
+		fn sub(self, other: &Polynomial<T>) -> Polynomial<T> { (-other) + self }
+	}
 }
 
 // SubAssign implementations
 // ===================================================================================
 
-impl SubAssign<&Polynomial> for Polynomial
+impl<T: Float + AddAssign> SubAssign<&Polynomial<T>> for Polynomial<T>
 {
-	fn sub_assign(&mut self, other: &Polynomial) { *self += -other; }
+	fn sub_assign(&mut self, other: &Polynomial<T>) { *self += -other; }
 }
 
 // Ownership taking version
-impl SubAssign<Polynomial> for Polynomial
+impl<T: Float + AddAssign> SubAssign<Polynomial<T>> for Polynomial<T>
 {
-	fn sub_assign(&mut self, other: Polynomial) { *self += -&other; }
+	fn sub_assign(&mut self, other: Polynomial<T>) { *self += -&other; }
 }
-// Number adding versions
-impl<T: ToPrimitive + Debug> SubAssign<T> for Polynomial
+// Number subtracting versions
+impl<F, T> SubAssign<T> for Polynomial<F>
+where
+	F: Float + AddAssign,
+	T: ToPrimitive + Neg,
+	<T as Neg>::Output: ToPrimitive,
 {
-	fn sub_assign(&mut self, other: T)
-	{
-		let x: f64 = match other.to_f64() {
-			Some(y) => y,
-			None => panic!("Error when converting {:?} to f64", other),
-		};
-		*self += -x;
-	}
+	fn sub_assign(&mut self, other: T) { *self += -other; }
 }
