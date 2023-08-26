3666a8ea1a43636b4389d2a1b0a1b89728d57a15
Silzinc <jonasbloch85@gmail.com>
2023-08-26 03:01:21 +0200

    Changed for generic type Polynomial

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,13 @@
-use crate::errors::PolynomialError;
-use crate::errors::PolynomialError::*;
-use crate::Polynomial;
-use num_traits::Pow;
-use std::fmt;
+use crate::{
+	convert,
+	errors::PolynomialError::{self, *},
+	Polynomial,
+};
+use num_traits::{Float, Pow};
+use std::{
+	fmt::{self, LowerExp},
+	ops::AddAssign,
+};
 use Polynomial::{NonZero, Zero, X};
 
 #[derive(PartialEq, Debug, Clone, Copy)]
@@ -45,9 +50,9 @@ impl Ops
 	}
 }
 
-struct Parser
+struct Parser<T: Float>
 {
-	pols_vec: Vec<Polynomial>,
+	pols_vec: Vec<Polynomial<T>>,
 	ops_vec: Vec<Ops>,
 	reads_num: bool,
 	reads_dec: bool,
@@ -58,14 +63,15 @@ struct Parser
 	is_factor: bool,
 }
 
-impl Parser
+impl<T: Float + AddAssign + LowerExp> Parser<T>
 {
-	fn execute_bin_operator(&mut self) -> Result<(), PolynomialError>
+	fn execute_bin_operator(&mut self) -> Result<(), PolynomialError<T>>
 	{
 		// println!("Got {:?} and {:?}", self.pols_vec, op);
 		let op: Ops = self.ops_vec.pop().ok_or(NoBinaryOperator)?;
-		let p1 = self.pols_vec.pop().ok_or(BinaryOperatorZeroOperand(op))?;
-		let p2 = self.pols_vec.pop().ok_or(BinaryOperatorOneOperand(op, p1.as_string()))?;
+		let p1: Polynomial<T> = self.pols_vec.pop().ok_or(BinaryOperatorZeroOperand(op))?;
+		let p2: Polynomial<T> = self.pols_vec.pop().ok_or(BinaryOperatorOneOperand(op, p1.as_string()))?;
+
 		if op == Ops::Mul
 			&& self.ops_vec.len() != 0
 			&& self.ops_vec[self.ops_vec.len() - 1] == Ops::Pow
@@ -75,56 +81,54 @@ impl Parser
 			&& p2 == X
 		{
 			// This is to optimize a bit the parsing of an expression like "cX^pow"
-			let c: f64 = self.pols_vec.pop().unwrap().coef(0);
-			let pow: f64 = p1.coef(0);
-			if pow == pow.round() && pow >= 0. {
-				let l = (pow as usize) + 1;
-				let mut coefs = vec![0f64; l];
+			let c: T = self.pols_vec.pop().unwrap().coef(0);
+			let pow: T = p1.coef(0);
+			if pow == pow.round() && pow.is_sign_positive() {
+				let l: usize = convert::<T, usize>(pow) + 1;
+				let mut coefs: Vec<T> = vec![convert(0); l];
 				coefs[l - 1] = c;
 				self.ops_vec.pop();
 				self.pols_vec.push(Polynomial::from(coefs));
 				Ok(())
 			} else {
-				Err(ImpossiblePower(p2.as_string(), c))
+				Err(ImpossiblePower(p2.as_string(), convert(c)))
 			}
 		} else {
 			match op {
 				Ops::Add => self.pols_vec.push(p2 + p1),
 				Ops::Min => self.pols_vec.push(p2 - p1),
 				Ops::Mul => self.pols_vec.push(p2 * p1),
-				Ops::Div => {
+				Ops::Div =>
 					if p1.degree() == 0 {
-						let c: f64 = 1. / p1.coef(0);
-						self.pols_vec.push(c * p2)
+						let c: T = convert::<i32, T>(1) / p1.coef(0);
+						self.pols_vec.push(p2 * c)
 					} else {
 						return Err(ImpossibleDivision(p2.as_string(), p1.as_string()));
-					}
-				}
-				Ops::Pow => {
+					},
+				Ops::Pow =>
 					if p1.degree() == 0 {
-						let c: f64 = p1.coef(0);
+						let c: T = p1.coef(0);
 						if p2.degree() == 0 {
-							self.pols_vec.push(Polynomial::from(vec![p2.coef(0).pow(c) as f64]))
+							self.pols_vec.push(Polynomial::from(vec![p2.coef(0).powf(c) as T]))
 						} else if c == c.round() {
 							if c.is_sign_negative() {
 								return Err(ImpossiblePower(p2.as_string(), c));
 							}
-							let r: Polynomial = p2.pow(c as i64).unwrap();
+							let r: Polynomial<T> = p2.pow(convert::<_, i64>(c)).unwrap();
 							self.pols_vec.push(r)
 						} else {
 							return Err(ImpossiblePower(p2.as_string(), c));
 						}
 					} else {
 						return Err(ImpossiblePower2Polynomials(p2.as_string(), p1.as_string()));
-					}
-				}
+					},
 				Ops::Open => return Err(ImpossibleOpen),
 			};
 			Ok(())
 		}
 	}
 
-	fn push_num(&mut self) -> Result<(), PolynomialError>
+	fn push_num(&mut self) -> Result<(), PolynomialError<T>>
 	{
 		if self.reads_num {
 			if self.is_factor {
@@ -135,9 +139,9 @@ impl Parser
 				self.pols_vec.push(Zero);
 			} else {
 				if self.is_min {
-					self.pols_vec.push(NonZero(vec![-(self.num as f64) / 10f64.pow(self.nb_decs as f64)]));
+					self.pols_vec.push(NonZero(vec![convert(-(self.num as f64) / 10f64.powi(self.nb_decs as i32))]));
 				} else {
-					self.pols_vec.push(NonZero(vec![(self.num as f64) / 10f64.pow(self.nb_decs as f64)]));
+					self.pols_vec.push(NonZero(vec![convert(self.num as f64 / 10f64.powi(self.nb_decs as i32))]));
 				}
 			}
 			self.num = 0;
@@ -150,7 +154,7 @@ impl Parser
 		Ok(())
 	}
 
-	fn push_bin_operator(&mut self, op: Ops) -> Result<(), PolynomialError>
+	fn push_bin_operator(&mut self, op: Ops) -> Result<(), PolynomialError<T>>
 	{
 		if self.is_min {
 			Err(UnaryMinusFailed(op))
@@ -176,7 +180,7 @@ impl Parser
 		self.unary_min = false;
 	}
 
-	fn read_char(&mut self, c: char) -> Result<(), PolynomialError>
+	fn read_char(&mut self, c: char) -> Result<(), PolynomialError<T>>
 	{
 		match c {
 			' ' => self.push_num()?,
@@ -185,8 +189,8 @@ impl Parser
 				self.push_bin_operator(Ops::Add)?;
 				self.is_factor = false;
 				self.unary_min = true;
-			}
-			'-' => {
+			},
+			'-' =>
 				if self.unary_min {
 					self.unary_min = false;
 					self.is_min = true;
@@ -196,26 +200,25 @@ impl Parser
 					self.push_bin_operator(Ops::Min)?;
 					self.is_factor = false;
 					self.unary_min = true;
-				}
-			}
+				},
 			'*' => {
 				self.push_num()?;
 				self.push_bin_operator(Ops::Mul)?;
 				self.is_factor = false;
 				self.unary_min = true;
-			}
+			},
 			'/' => {
 				self.push_num()?;
 				self.push_bin_operator(Ops::Div)?;
 				self.is_factor = false;
 				self.unary_min = true;
-			}
+			},
 			'^' => {
 				self.push_num()?;
 				self.push_bin_operator(Ops::Pow)?;
 				self.is_factor = false;
 				self.unary_min = true;
-			}
+			},
 			'X' | 'x' => {
 				self.push_num()?;
 				if self.is_factor {
@@ -227,11 +230,11 @@ impl Parser
 					// In that case we have an expression like -X, which should only be seen at the
 					// beginning of an expression
 					self.is_min = false;
-					self.pols_vec.push(NonZero(vec![-1f64]));
+					self.pols_vec.push(NonZero(vec![convert(-1)]));
 					self.push_bin_operator(Ops::Mul)?;
 				}
 				self.pols_vec.push(X);
-			}
+			},
 			'(' => {
 				self.push_num()?;
 				if self.is_factor {
@@ -241,13 +244,13 @@ impl Parser
 				if self.is_min {
 					// The idea is to turning any (...) into ((...)) and -(...) into (-1 * (...))
 					self.is_min = false;
-					self.pols_vec.push(NonZero(vec![-1f64]));
+					self.pols_vec.push(NonZero(vec![convert(-1)]));
 					self.push_bin_operator(Ops::Mul)?;
 				}
 				self.ops_vec.push(Ops::Open);
 				self.is_factor = false;
 				self.unary_min = true;
-			}
+			},
 			')' => {
 				self.push_num()?;
 				while self.ops_vec.len() != 0 && self.ops_vec[self.ops_vec.len() - 1] != Ops::Open {
@@ -266,17 +269,17 @@ impl Parser
 				self.is_min = false;
 				self.is_factor = true;
 				self.unary_min = false;
-			}
+			},
 			'.' => {
 				self.reads_num = true;
 				self.reads_dec = true;
 				self.unary_min = false;
-			}
+			},
 			',' => {
 				self.reads_num = true;
 				self.reads_dec = true;
 				self.unary_min = false;
-			}
+			},
 			'0' => self.read_digit(0),
 			'1' => self.read_digit(1),
 			'2' => self.read_digit(2),
@@ -290,52 +293,52 @@ impl Parser
 			'⁰' => {
 				self.read_char('^')?;
 				self.read_digit(0);
-			}
+			},
 			'¹' => {
 				self.read_char('^')?;
 				self.read_digit(1);
-			}
+			},
 			'²' => {
 				self.read_char('^')?;
 				self.read_digit(2);
-			}
+			},
 			'³' => {
 				self.read_char('^')?;
 				self.read_digit(3);
-			}
+			},
 			'⁴' => {
 				self.read_char('^')?;
 				self.read_digit(4);
-			}
+			},
 			'⁵' => {
 				self.read_char('^')?;
 				self.read_digit(5);
-			}
+			},
 			'⁶' => {
 				self.read_char('^')?;
 				self.read_digit(6);
-			}
+			},
 			'⁷' => {
 				self.read_char('^')?;
 				self.read_digit(7);
-			}
+			},
 			'⁸' => {
 				self.read_char('^')?;
 				self.read_digit(8);
-			}
+			},
 			'⁹' => {
 				self.read_char('^')?;
 				self.read_digit(9);
-			}
+			},
 			_ => return Err(UnsupportedCharacter(c)),
 		};
 		Ok(())
 	}
 }
 
-fn _parse_string(s: String) -> Result<Polynomial, PolynomialError>
+fn _parse_string<T: Float + AddAssign + LowerExp>(s: String) -> Result<Polynomial<T>, PolynomialError<T>>
 {
-	let mut parser = Parser {
+	let mut parser = Parser::<T> {
 		pols_vec: Vec::new(),
 		ops_vec: Vec::new(),
 		reads_num: false,
@@ -357,7 +360,7 @@ fn _parse_string(s: String) -> Result<Polynomial, PolynomialError>
 	parser.pols_vec.pop().ok_or(EmptyStringInput)
 }
 
-pub fn parse_string(s: String) -> Result<Polynomial, String>
+pub fn parse_string<T: Float + AddAssign + LowerExp>(s: String) -> Result<Polynomial<T>, String>
 {
 	match _parse_string(s) {
 		Ok(p) => Ok(p),
