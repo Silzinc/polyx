c64d6350deacb768e486b9b16cb800b3ed934ab9
Silzinc <jonasbloch85@gmail.com>
2023-08-30 20:27:17 +0200

    Adapted Bernstein to the refactor

--- src/specific/bernstein.rs
+++ src/specific/bernstein.rs
@@ -1,44 +1,37 @@
-use crate::convert;
-use crate::Polynomial::{self, NonZero, Zero};
-use num_traits::{Float, Pow, PrimInt, ToPrimitive};
+use crate::Polynomial;
+use num_traits::{One, Zero};
+use std::ops::{Mul, Sub};
 
-fn binom<T: PrimInt, U1: PrimInt, U2: PrimInt>(_n: U1, _k: U2) -> T
+fn binom<T: From<usize>, U1: Into<usize>, U2: Into<usize>>(_n: U1, _k: U2) -> T
 {
-	let n = _n.to_u32().unwrap();
-	let k = _k.to_u32().unwrap();
+	let n: usize = _n.into();
+	let k: usize = _k.into();
 	if k > n {
-		convert(0)
+		T::from(0)
 	} else {
-		let mut b: u32 = convert(1);
+		let mut b: usize = 1;
 		for j in 0..k {
 			b *= n - j;
 		}
 		for j in 1..=k {
 			b /= j;
 		}
-		convert(b)
+		T::from(b)
 	}
 }
 
-impl<T: Float> Polynomial<T>
+impl<T> Polynomial<T>
+where T: Mul<T, Output = T> + Sub<T, Output = T> + Clone + Zero + One + PartialEq + From<usize>
 {
-	pub fn bernstein<U: ToPrimitive>(_m: U, _i: U) -> Self
+	pub fn bernstein<U: Into<usize>>(_m: U, _i: U) -> Self
 	{
-		let m = _m.to_usize().unwrap();
-		let i = _i.to_usize().unwrap();
+		let m: usize = _m.into();
+		let i: usize = _i.into();
 		if i > m {
-			Zero
+			Self::zero()
 		} else {
-			let b: T = convert(binom::<u32, _, _>(m, i));
-			let v: Vec<T> = match NonZero(vec![convert(1), convert(-1)]).pow(m - i) {
-				Ok(NonZero(coefs)) => coefs,
-				_ => unreachable!(), // Won't happen since m >= i
-			};
-			let mut coefs = vec![convert(0); m + 1];
-			for k in i..=m {
-				coefs[k] = b * v[k - i];
-			}
-			NonZero(coefs)
+			let b: T = binom::<usize, _, _>(m, i).into();
+			crate::polynomial![T::one(), T::zero() - T::one()].powi(m - i).into_iter().map(|x| x * b).collect::<Polynomial<T>>() * crate::polynomial![T::zero(), T::one()].powi(i)
 		}
 	}
 }
