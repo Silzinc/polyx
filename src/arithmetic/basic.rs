2dbae28bdadebf56dc8c8dd90d1acbe13aa31768
Silzinc <jonasbloch85@gmail.com>
2023-08-30 19:53:31 +0200

    Corrected some bugs and added some methods

--- src/arithmetic/basic.rs
+++ src/arithmetic/basic.rs
@@ -1,70 +1,89 @@
 use crate::Polynomial;
 use num_traits::{One, Zero};
-use std::ops::{Add, Mul, Index, IndexMut};
+use std::ops::{Add, Index, IndexMut, Mul};
 
 // Some basic implementations needed elsewhere
 
 impl<T> Index<usize> for Polynomial<T>
 {
-  // Gives the coefficient of the monomial of degree index
-  type Output = T;
-  #[inline]
-  fn index(&self, index: usize) -> &Self::Output
-  {
-    (self.0)[index]
-  }
+	// Gives the coefficient of the monomial of degree index
+	/* Example:
+	let p = polynomial![1, 0, 2];
+	assert_eq!(p[0], 1);
+	*/
+	type Output = T;
+	#[inline]
+	fn index(&self, index: usize) -> &Self::Output { &(self.0)[index] }
 }
 
 impl<T> IndexMut<usize> for Polynomial<T>
 {
-  #[inline]
-  fn index_mut(&mut self, index: usize) -> &mut Self::Output
-  {
-    &mut (self.0)[index]
-  }
+	#[inline]
+	fn index_mut(&mut self, index: usize) -> &mut Self::Output { &mut (self.0)[index] }
 }
 
-macro_rules! impl_iter
-{
-  // Implementations to iterate over the coefficients of a polynomial
-  ($t:ty, $iter_type:ty, $method:ident) => {
-    impl<T> IntoIterator for $t
-    {
-      type Item = T;
-      type IntoIter = $iter_type;
-      #[inline]
-      fn into_iter(self) -> Self::IntoIter
-      {
-        self.0.$method()
-      }
-    }
-  }
+macro_rules! impl_iter {
+	// Implementations to iterate over the coefficients of a polynomial
+	($t:ty, $item_spec:ty, $iter_type:ty, $method:ident) => {
+		impl<'a, T> IntoIterator for $t
+		{
+			type Item = $item_spec;
+			type IntoIter = $iter_type;
+			#[inline]
+			fn into_iter(self) -> Self::IntoIter { self.0.$method() }
+		}
+	};
 }
 
-impl_iter!(Polynomial<T>, std::vec::IntoIter<T>, into_iter);
-impl_iter!(&Polynomial<T>, std::slice::Iter<T>, iter);
-impl_iter!(&mut Polynomial<T>, std::slice::IterMut<T>, iter_mut);
+impl_iter!(Polynomial<T>, T, std::vec::IntoIter<T>, into_iter);
+impl_iter!(&'a Polynomial<T>, &'a T, std::slice::Iter<'a, T>, iter);
+impl_iter!(&'a mut Polynomial<T>, &'a mut T, std::slice::IterMut<'a, T>, iter_mut);
 
 impl<T> Polynomial<T>
 {
-  // Gives the degree of self
-  // Note that the degree of the zero polynomial is 0 to avoid usize underflow
-  // The is_zero() method is preferred to distinguish the zero polynomial from the other constant polynomials
-  #[inline]
-	pub fn degree(&self) -> usize { if self.is_zero() 0 else self.0.len() - 1 }
+	// Gives the degree of self
+	// Note that the degree of the zero polynomial is 0 to avoid usize underflow
+	// The is_zero() method is preferred to distinguish the zero polynomial from the
+	// other constant polynomials
+	/* Example:
+	let p = polynomial![1, 0, 2];
+	assert_eq!(p.degree(), 2);
+	*/
+	#[inline]
+	pub fn degree(&self) -> usize
+	{
+		if self.0.len() == 0 {
+			0
+		} else {
+			self.0.len() - 1
+		}
+	}
+}
+
+impl<T> Polynomial<T>
+where T: One + Zero + PartialEq
+{
+	#[inline]
+	pub fn is_x(&self) -> bool { self.degree() == 1 && self[1] == T::one() && self[0] == T::zero() }
 }
 
 impl<T> Polynomial<T>
 where T: Zero + Mul<T, Output = T> + Add<T, Output = T>
 {
-  #[inline]
+	// Evaluates self(x)
+	/* Example:
+	let p = polynomial![1, 0, 2];
+	assert_eq!(p.eval(2), 9);
+	*/
+	#[inline]
 	pub fn eval<U: Into<T>>(&self, _x: U) -> T
 	{
 		// Computes self(x)
 		let x: T = _x.into();
 		let mut result = T::zero();
 		for coef in self.into_iter().rev() {
-			result = x * result + coef;
+			result = x * result + *coef;
 		}
+		result
 	}
 }
