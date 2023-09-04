e01795d2082358886cb7827f7347abfff32acd1d
Silzinc <jonasbloch85@gmail.com>
2023-09-04 12:22:42 +0200

    Changed traits requirements

--- src/specific/bernstein.rs
+++ src/specific/bernstein.rs
@@ -1,14 +1,13 @@
 use crate::Polynomial;
-use num_traits::{One, Zero};
+use num_traits::{PrimInt, Zero};
 use std::fmt::Debug;
-use std::ops::{Mul, Sub};
 
-fn binom<T: From<usize>, U1: Into<usize>, U2: Into<usize>>(_n: U1, _k: U2) -> T
+fn binom<T: PrimInt, U1: PrimInt, U2: PrimInt>(_n: U1, _k: U2) -> T
 {
-	let n: usize = _n.into();
-	let k: usize = _k.into();
+	let n: usize = _n.to_usize().unwrap();
+	let k: usize = _k.to_usize().unwrap();
 	if k > n {
-		T::from(0)
+		T::from(0).unwrap()
 	} else {
 		let mut b: usize = 1;
 		for j in 0..k {
@@ -17,27 +16,25 @@ fn binom<T: From<usize>, U1: Into<usize>, U2: Into<usize>>(_n: U1, _k: U2) -> T
 		for j in 1..=k {
 			b /= j;
 		}
-		T::from(b)
+		T::from(b).unwrap()
 	}
 }
 
-impl<T> Polynomial<T>
-where T: Mul<T, Output = T> + Sub<T, Output = T> + Clone + Zero + One + PartialEq + From<usize> + Debug
+impl<T> Polynomial<T> where T: Debug + PrimInt
 {
-	pub fn bernstein<U: Into<usize>>(_m: U, _i: U) -> Self
+	pub fn bernstein<U: PrimInt>(_m: U, _i: U) -> Self
 	{
-		let m: usize = _m.into();
-		let i: usize = _i.into();
+		let m: usize = _m.to_usize().unwrap();
+		let i: usize = _i.to_usize().unwrap();
 		if i > m {
 			Self::zero()
 		} else {
-			let b: T = binom::<usize, _, _>(m, i).into();
-			crate::polynomial![T::one(), T::zero() - T::one()]
-				.powi(m - i)
-				.into_iter()
-				.map(|x| x * b.clone())
-				.collect::<Polynomial<T>>()
-				* crate::polynomial![T::zero(), T::one()].powi(i)
+			let b = T::from(binom::<usize, _, _>(m, i)).unwrap();
+			crate::polynomial![T::one(), T::zero() - T::one()].powi(m - i)
+			                                                  .into_iter()
+			                                                  .map(|x| x * b.clone())
+			                                                  .collect::<Polynomial<T>>()
+			* crate::polynomial![T::zero(), T::one()].powi(i)
 		}
 	}
 }
--- src/specific/lagrange.rs
+++ src/specific/lagrange.rs
@@ -4,23 +4,34 @@ use std::fmt::Debug;
 use std::ops::{Div, Mul, Neg, Sub};
 
 impl<T> Polynomial<T>
-where T: Mul<T, Output = T> + Sub<T, Output = T> + Div<T, Output = T> + Clone + Zero + One + Neg<Output = T> + Debug
+	where T: Mul<T, Output = T>
+	        + Sub<T, Output = T>
+	        + Div<T, Output = T>
+	        + Clone
+	        + Zero
+	        + One
+	        + Neg<Output = T>
+	        + Debug
 {
-	pub fn lagrange<F1, F2>(_x_array: &Vec<F1>, _values: &Vec<F2>) -> Self
-	where
-		F1: Into<T> + Clone,
-		F2: Into<T> + Clone,
+	pub fn lagrange<F1, F2>(_x_array: &[F1], _values: &[F2]) -> Self
+		where F1: Into<T> + Clone,
+		      F2: Into<T> + Clone
 	{
 		// Computes the interpolating polynomial of minimal degree between the two
 		// arrays with the Lagrange barycentric method
+		// NOTE : it is not recommended to use this method if T is an integer type
 		#[allow(non_snake_case)]
 		let X = crate::polynomial![T::zero(), T::one()];
 		let n = _x_array.len(); // Output will be of degree n - 1
 		if n != _values.len() {
-			panic!("Cannot make polynomial interpolation from {} points and {} values", n, _values.len());
+			panic!(
+			       "Cannot make polynomial interpolation from {} points and {} values",
+			       n,
+			       _values.len()
+			);
 		}
 		let mut weights = vec![T::one(); n];
-		let mut factors: Vec<Polynomial<T>> = vec![crate::polynomial![T::one()]; n];
+		let mut factors = vec![crate::polynomial![T::one()]; n];
 		let x_array: Vec<T> = _x_array.iter().map(|x| x.clone().into()).collect();
 		let values: Vec<T> = _values.iter().map(|x| x.clone().into()).collect();
 		for j in 0..(n - 1) {
@@ -34,13 +45,14 @@ where T: Mul<T, Output = T> + Sub<T, Output = T> + Div<T, Output = T> + Clone + 
 				weights[k] = weights[k].clone() * (x_array[k].clone() - x_array[j].clone());
 			}
 		}
+		// Now when j = n - 1
 		for k in 0..(n - 1) {
 			factors[k] = &X * &factors[k] - crate::polynomial![x_array[n - 1].clone()] * &factors[k];
 			weights[k] = weights[k].clone() * (x_array[k].clone() - x_array[n - 1].clone());
 		}
 		let mut result = Polynomial::zero();
 		for k in 0..n {
-			result += crate::polynomial![values[k].clone() / weights[k].clone()] * factors[k].clone();
+			result += crate::polynomial![values[k].clone() / weights[k].clone()] * &factors[k];
 		}
 		result
 	}
