757224aa2fb60f9bedb3a20f42d80f562fd41c88
Silzinc <jonasbloch85@gmail.com>
2023-09-16 13:28:33 +0200

    Implemented euclidean division algorithm.

--- src/ops/div_algos.rs
+++ src/ops/div_algos.rs
@@ -1,5 +1,5 @@
 use crate::Polynomial;
-use num_traits::{Float, Signed};
+use num_traits::{Float, Signed, Zero};
 use std::fmt::Debug;
 
 impl<T> Polynomial<T> where T: Clone + Debug + Signed
@@ -7,14 +7,14 @@ impl<T> Polynomial<T> where T: Clone + Debug + Signed
 	// Implements an algorithm to invert a polynomial modulo an integer
 	// Based on https://thibautverron.github.io/enseignement/2018-CompAlg2-notes.pdf, page 26
 	// Time complexity: O(M(n)), where M(n) is the time complexity of the
-	// multiplication algorithm
+	// multiplication algorithm and n the degree of u
 	// Space complexity: O(n)
 	/* Example:
 	let p = polynomial![1, -4, 0, -2, 5, 1, 1, 1];
 	let inv10 = Polynomial::inverse(&p, 10);
 	assert_eq!(Polynomial::short_product(&p, &inv10, 10),
 						 Polynomial::from(1));
-	 */
+	*/
 	// Be careful with this function, overflows can happen pretty easily (i32 might
 	// not be big enough)
 
@@ -52,5 +52,70 @@ impl<T> Polynomial<T> where T: Clone + Debug + Float
 			result_modulus <<= 1;
 		}
 		Polynomial::from(v)
+	}
+}
+
+impl<T> Polynomial<T> where T: Clone + Debug + Signed
+{
+	// Implements the Euclidean division of two polynomials
+	// Based on https://thibautverron.github.io/enseignement/2018-CompAlg2-notes.pdf, page 26
+	// Time complexity: O(M(m)), where M(m) is the time complexity of the
+	// multiplication algorithm and m the degree of p1
+	/* Example:
+	let mut a = polynomial![1, 0, 2];
+	let mut b = polynomial![1, 1];
+	let (q, r) = Polynomial::euclidean_division(&mut a, &mut b);
+
+	assert_eq!(b * q + r, a);
+	*/
+	pub fn euclidean_division(p1: &mut Self, p2: &mut Self) -> (Self, Self)
+	{
+		if p2.is_zero() {
+			panic!("Polynomial division by zero");
+		}
+		if p1.degree() < p2.degree() {
+			return (Self::zero(), p1.clone());
+		}
+		let m = p1.degree();
+		let n = p2.degree();
+		p1.rev_inplace();
+		p2.rev_inplace();
+		let h = Self::inverse(&p2, m - n + 1);
+		let mut q = Self::short_product(&p1, &h, m - n + 1);
+		p1.rev_inplace();
+		p2.rev_inplace();
+		q.rev_inplace();
+
+		let r = &*p1 - &*p2 * &q;
+		(q, r)
+	}
+
+	// This version forces to create a copy of p1 and p2 and is therefore less
+	// efficient, but allows for immutable arguments
+	/* Example:
+	let a = polynomial![1, 0, 2];
+	let b = polynomial![1, 1];
+	let (q, r) = Polynomial::euclidean_division_immutable(&a, &b);
+
+	assert_eq!(b * q + r, a);
+	*/
+	pub fn euclidean_division_immutable(p1: &Self, p2: &Self) -> (Self, Self)
+	{
+		if p2.is_zero() {
+			panic!("Polynomial division by zero");
+		}
+		if p1.degree() < p2.degree() {
+			return (Self::zero(), p1.clone());
+		}
+		let m = p1.degree();
+		let n = p2.degree();
+		let p1_rev = p1.rev();
+		let p2_rev = p2.rev();
+		let h = Self::inverse(&p2_rev, m - n + 1);
+		let mut q = Self::short_product(&p1_rev, &h, m - n + 1);
+		q.rev_inplace();
+
+		let r = p1 - p2 * &q;
+		(q, r)
 	}
 }
